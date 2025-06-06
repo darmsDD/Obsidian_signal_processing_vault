82ed209534ae040bf6d67c9c1b818dc39f7bb1f2
darmsDD <ivandinizdobbin2@gmail.com>
2025-06-05 21:07:36 -0300

    vault backup: 2025-06-05 21:07:36

--- Lista5/Estimar_autoespectros_e_espectro_cruzado_entre2sinais.m
+++ Lista5/Estimar_autoespectros_e_espectro_cruzado_entre2sinais.m
@@ -1,106 +1,149 @@
 % gera��o dos sinais de entrada e sa�da
 clear all
 close all
+
+%% Define x, dt e o vetor do tempo t
+
 N=128*1024;
 x=randn(1,N);
 dt=1e-3;
 t=0:dt:(N-1)*dt;
+    
 
+%% Calculo da fft(x), define o periodo T e o vetor de frequencia f
+X=fft(x);
+T=N*dt;
+df=1/T;
+f=(0:N/2-1)*df;
+
+%% Calcula a funçao de resposta em frequencia H(f)
+w=2*pi*f;
+%M*y''+C*y'+K*y=x
+M=1;K=100000;C=50;
+H=1./(K-w.^2*M+1i*w*C);
+
+%% Calcula a saida do sistema pela multiplicaçao de H(f) e X(f)
+
+Y=H.*X(1:N/2);
+% Y foi criada usando apenas as frequências positivas de X
+% porém para reconstruir o sinal também precisamos da frequências negativas
+% Sabemos que um |X(f)| qualquer [3 1 2 0 2 1] é uma função par.
+% porém geralmente temos ela no formato [0 2 1 3 1 2], isto é,
+% primeiro as frequÊncias positivas e depois negativas.
+% Também sabemos da propriedade X(-f) = X*(f) {conjugado}
+% Exemplo com apenas frequÊncias positivas:
+% Y(f) = [0 2+i 1-i] Y*(f)(2:N/2) = [2-i 1+i] flipr(Y*(f)) = [1+i 2-i]
+% Y(f) = [Y(f) 0 Y*(f)] = [0 2+i 1-i 0 1+i 2-i]
+% A adição do 0 ainda não está clara para mim. Talvez nyquist.
+% Fazendo fftshift (só para ver o resultado final)
+% [0 1+i 2-i 0 2+i 1-i]
+
+Y=[Y 0 fliplr(conj(Y(2:N/2)))];
+y=ifft(Y);
+
+
+
+
+%% Define o vetor de ruido
 N_x_noise_cases = 3;
 N_y_noise_cases = 3;
+noise_multiplier = 5;
 noisex_array = zeros(1,N_x_noise_cases);
 noisey_array = zeros(1,N_y_noise_cases);
 
 for i=1:N_x_noise_cases
-    noisex_array(i) = (i-1)*5;
+    noisex_array(i) = (i-1)*noise_multiplier;
 end
 for i=1:N_y_noise_cases
-    noisey_array(i) = (i-1)*5;
+    noisey_array(i) = (i-1)*noise_multiplier;
 end
 
 x_without_noise = x;
-for i=1:N_y_noise_cases
-    for j=1:N_x_noise_cases
-        noisex = noisex_array(j);
-        noisey = noisey_array(i);
 
-        x=x_without_noise;
+%% Testa o sistema com diversos ruidos
+for i=1:N_x_noise_cases
+    for j=1:N_y_noise_cases
+        noisex = noisex_array(i);
+        noisey = noisey_array(j);
+        % reseta o x para antes de ser adicionado um ruido
+        x = x_without_noise; %
         
-        X=fft(x);
-        T=N*dt;
-        df=1/T;
-        f=(0:N/2-1)*df;
-        w=2*pi*f;
-        %M*y''+C*y'+K*y=x
-        M=1;K=100000;C=50;
-        H=1./(K-w.^2*M+1i*w*C);
-        
-        Y=H.*X(1:N/2);
-        % Y foi criada usando apenas as frequências positivas de X
-        % porém para reconstruir o sinal também precisamos da frequências negativas
-        % Sabemos que um |X(f)| qualquer [3 1 2 0 2 1] é uma função par.
-        % porém geralmente temos ela no formato [0 2 1 3 1 2], isto é,
-        % primeiro as frequÊncias positivas e depois negativas.
-        % Também sabemos da propriedade X(-f) = X*(f) {conjugado}
-        % Exemplo com apenas frequÊncias positivas:
-        % Y(f) = [0 2+i 1-i] Y*(f)(2:N/2) = [2-i 1+i] flipr(Y*(f)) = [1+i 2-i]
-        % Y(f) = [Y(f) 0 Y*(f)] = [0 2+i 1-i 0 1+i 2-i]
-        % A adição do 0 ainda não está clara para mim. Talvez nyquist.
-        % Fazendo fftshift (só para ver o resultado final)
-        % [0 1+i 2-i 0 2+i 1-i]
-        
-        Y=[Y 0 fliplr(conj(Y(2:N/2)))];
-        y=ifft(Y);
-        
+
+        %=========Adiciona ruido a entrada===========
         %============================================
-        %========Adding noise to exit ==============
+        wn=noisex*rms(x)*randn(1,N);
+        x = x+wn;
+        X_noise=fft(x);
+        figure;
+        subplot(4,1,3);
+        hold on;
+        plot(f,abs(X(1:N/2))/N);
+        plot(f,abs(X_noise(1:N/2))/N);
+        legend("X(f)","X noise(f)");
+        ylabel("Amplitude",'FontWeight', 'bold');
+        xlabel("f(Hz)",'FontWeight', 'bold');
+        hold off;
+       
+
+        %============================================
+        %============================================
+
+
+
+        %=========Adiciona ruido a saida=============
+        %============================================
         %percY=input('ruido na saida (0-100):    ');
         percY=noisey/100;
         zn=percY*rms(y)*randn(1,N);
         y=y+zn;
+        Y_noise=fft(y);
+        subplot(4,1,4);
+        hold on;
+        plot(f,abs(Y(1:N/2))/N);
+        plot(f,abs(Y_noise(1:N/2))/N);
+        legend("Y(f)","Y noise(f)");
+        ylabel("Amplitude",'FontWeight', 'bold');
+        xlabel("f(Hz)",'FontWeight', 'bold');
+        hold off;
         %============================================
         %============================================
         
-         %====================================
-        % Adding noise to entrance
-        percX=70/100;
-        wn=percX*rms(x)*randn(1,N);
-        x = x+wn;  
-        %==================================
 
-
-
-        Nb=1024;
-        nb=128;
-        Tb=Nb*dt;
-        dfb=1/Tb;
-        fb=(0:Nb/2-1)*dfb;
-        wb=2*pi*fb;
-        [Sxx,Syy,Sxy, Syx]=depxyper(x,y,dt,Nb);
+        Nb=1024; % numero de blocos
+        nb=128;  % numero de pontos por bloco
+        Tb=Nb*dt; % periodo do bloco
+        dfb=1/Tb; % delta f do bloco
+        fb=(0:Nb/2-1)*dfb; % vetor de frequencias fb
+         
+        %% Calculo das densidades espectrais de potencia
+        [Sxx,Syy,Sxy]=depxyper(x,y,dt,Nb);
         H1=Sxy./Sxx;
-        H2=Syy./Syx;
+        H2=Syy./conj(Sxy);
+        
+        %% Calculo da funcao de transferencia Hb
+        wb=2*pi*fb;
+        Hb=1./(K-wb.^2*M+1i*wb*C);
+        
+        %% Calculo da funçao de coerencia ordinaria
         Coh2=abs(Sxy).^2./(Sxx.*Syy);
-        figure;
+
+        %% Plot dos graficos Hb vs H1 vs H2 e coerencia ordinaria
         sgtitle(sprintf('X Noise = %d,Y Noise=%d/100', noisex,noisey),'FontWeight', 'bold');
-        subplot(2,1,1);
+        subplot(4,1,1);
         hold on
-        plot(f,20*log10(abs(H)),'b','LineWidth', 1.5);
+        plot(fb,20*log10(abs(Hb(1:Nb/2))),'b','LineWidth', 1.5);
         plot(fb,20*log10(abs(H1(1:Nb/2))),'LineWidth', 1);
         plot(fb, 20*log10(abs(H2(1:Nb/2))),'LineWidth', 1)
         ylabel("Amplitude",'FontWeight', 'bold');
         xlabel("f(hz)",'FontWeight', 'bold');
+        legend('Hb','H1','H2','Location', 'southwest');
         
-        %plot(fb,20*log10(abs(H(1:npt/2))),'b',f,20*log10(abs(H1)),'-',f,20*log10(abs(H2)),'-')
-        
-        
-        legend('H','H1','H2','Location', 'southwest');
-        subplot(2,1,2);
+        subplot(4,1,2);
         plot(fb,Coh2(1:Nb/2))
         title("Coeficiente de coerência",'FontWeight', 'bold');
         ylabel("\gamma^2_{xy}(f)",'FontWeight', 'bold');
         xlabel("f(hz)",'FontWeight', 'bold');
         ylim([0 1.2])
 
-        
     end
 end
--- Lista5/Lista 5.md
+++ Lista5/Lista 5.md
@@ -1,42 +1,54 @@
 
-### Sem Ruído
+## Sem ruído
 
-Sem nenhum ruído, o coeficiente de coerência é praticamente 1 sempre. Veja também que os estimadores se aproximaram muito bem da função de transferência.
+
+Para o caso sem ruído, acontece o esperado, no qual H1 e H2 são imperceptíveis de Hb e a coerência ordinária é praticamente unitária.
+Além disso, não é possível notar a presença do ruído no espectro da frequência.
+
 
 ![[noisex_0_noisey_0_100.png]]
 
+## Ruído só na entrada
 
- 
-
-### Ruído só na saída
-
-Quanto mais eu aumento o meu ruído na saída, mais espaçado e com maior amplitude ficam os estimadores H1 e H2.  Além disso, é possível ver claramente que H2 foi muito mais afetado por este ruído.
-
-H2 é o limite superior e H1 o limite inferior. Então, H1 deve ser sempre menor ou igual a H2, isto é, $H1 \leq H2$. Este comportamento pode ser observado nas imagens abaixo.
-	
-Além disso, o coeficiente de coerência que começa em 1 também decai rapidamente com quantidades maiores de ruído.
-
-![[noisex_0_noisey_5_100.png]]
-
-![[noisex_0_noisey_10_100.png]]
-
-
-###  Ruído só na entrada
-
-Não está claro porque o ruído apenas na entrada não afeta, ou aparenta  não afetar, os estimadores H1 e H2. O coeficiente de coerência também parece não ter sido afetado.
+Percebe-se que a coerência ordinária caiu bruscamente ao adicionar ruído na entrada, chegando próximo a zero. Esse comportamento pode ser devido a alta presença do ruído no espectro da frequência (X noise). Além disso, conseguimos observar H2 e H1 como os limites superiores e inferiores da função de reposta em frequência Hb. Com H2 sendo um estimador melhor no início.
 
 
 ![[noisex_5_noisey_0_100.png]]
 ![[noisex_10_noisey_0_100.png]]
 
 
-### Ruído na entrada e na saída
 
-A combinação de ruídos na entrada e saída afetou os estimadores H1 e H2.
+## Ruído só na saída
 
-O ruído de entrada, mesmo parecendo não afetar os estimadores H1 e H2, teve efeitos.
-Os valores de H1 e H2 apresentam valores diferentes, para um mesmo ruído em y, mas diferentes ruídos em x.
+A coerência ordinária se manteve mais próxima de 1 no início e caiu mais rapidamente de acordo com a quantide de ruído inserido. Neste caso, H1 foi um estimador melhor. Perceba que a presença do ruído não é muito notada no espectro da frequência.
+
+
+![[noisex_0_noisey_5_100.png]] 
+![[noisex_0_noisey_10_100.png]]
+
+
+
+
+
+## Ruído na entrada e na saída
+
+
+Com a adição de ruído na entrada e na saída, a coerência ordinária despencou, se aproximando cada vez mais de 0, conforme cresce os ruídos.
+
+Perceba que parecido com o caso de ruído apenas na entrada, H2 foi um melhor estimador no inicío. Além disso, como visto antes, fica claro pelas imagens os comportamentos como limites superior (H2) e inferior (H1).
 
 
 ![[noisex_5_noisey_5_100.png]]
-![[noisex_5_noisey_10_100.png]]![[noisex_10_noisey_5_100.png]]![[noisex_10_noisey_10_100.png]]
+
+![[noisex_5_noisey_10_100.png]]
+
+
+![[noisex_10_noisey_5_100.png]]
+
+
+![[noisex_10_noisey_10_100.png]]
+
+
+## Observação final
+
+Não consegui demonstrar, mas pelo comportamento ruídoso da entrada X não se propagar para a saída Y, supõe-se que a função de resposta em frequência H(f) está atuando como um filtro.
