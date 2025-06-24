e981e8bf51823b6af9dcc60c9ae10efed93de529
darmsDD <ivandinizdobbin2@gmail.com>
2025-06-24 19:45:11 -0300

    vault backup: 2025-06-24 19:45:11

--- Trabalho/Codigos/Analise_modal_viga_livre_blevins.m
+++ Trabalho/Codigos/Analise_modal_viga_livre_blevins.m
@@ -181,42 +181,47 @@ vector_h_experimental = ifft(vector_H_experimental_add_f_negativo,[],1);
 
 [qtd_linhas_experimental,qtd_colunas_experimental] = size(vector_H_experimental_add_f_negativo);
 t = (0:qtd_linhas_experimental-1)*dt;
-plot_ht(vector_h_analitico,vector_h_experimental,t,tamanho_fila,vector_nome_das_pessoas);
+%plot_ht(vector_h_analitico,vector_h_experimental,t,tamanho_fila,vector_nome_das_pessoas);
 
 
 %% Prony
-tamanho_fila = 9;
-[array_A,array_s,nmod] = prony_e_graficos(vector_h_analitico,vector_h_experimental,tamanho_fila,t,nmod);
-     array_A = array_A';
-     array_wn = abs(array_s);
-    
-    
-    [l,c] = size(array_wn);
-    prony_modal = [];
-    prony_modaln = [];
-    for i=1:l
-         for j=1:c
-            [value, min_idx] = min(abs(array_wn(i,j)/(2*pi) - fn));
-            if(abs(value) < 1000)
-               prony_modal(min_idx,i) = abs(array_A(i,j)); 
-            end
-    
-         end
-    end
-    prony_modal_min = min(prony_modal(:));
-    prony_modal_max = max(prony_modal(:));
-    
-    prony_modal_norm = 2 * (prony_modal - prony_modal_min) / (prony_modal_max - prony_modal_min) - 1;
-    distances = (90e-3:70e-3:770e-3);
-    [~,tamanho_distancia] = size(prony_modal);
-    for i=1:length(fn)
-        figure;
-        hold on;
-        plot(distances(1:tamanho_distancia),prony_modal(i,:));
-        plot(distances(1:tamanho_distancia),phi(90e-3/dx:70e-3/dx:650e-3/dx,i));
-        legend("prony","analitico");
-        hold off;
-    end
+[array_s,array_A] = newpronySIMO(vector_h_experimental,dt,8);
+array_s = array_s';
+
+
+
+%tamanho_fila = 9;
+% [array_A,array_s,nmod] = prony_e_graficos(vector_h_analitico,vector_h_experimental,tamanho_fila,t,nmod);
+%      array_A = array_A';
+%      array_wn = abs(array_s);
+% 
+% 
+%     [l,c] = size(array_wn);
+%     prony_modal = [];
+%     prony_modaln = [];
+%     for i=1:l
+%          for j=1:c
+%             [value, min_idx] = min(abs(array_wn(i,j)/(2*pi) - fn));
+%             if(abs(value) < 1000)
+%                prony_modal(min_idx,i) = abs(array_A(i,j)); 
+%             end
+% 
+%          end
+%     end
+%     prony_modal_min = min(prony_modal(:));
+%     prony_modal_max = max(prony_modal(:));
+% 
+%     prony_modal_norm = 2 * (prony_modal - prony_modal_min) / (prony_modal_max - prony_modal_min) - 1;
+%     distances = (90e-3:70e-3:770e-3);
+%     [~,tamanho_distancia] = size(prony_modal);
+%     for i=1:length(fn)
+%         figure;
+%         hold on;
+%         plot(distances(1:tamanho_distancia),prony_modal(i,:));
+%         plot(distances(1:tamanho_distancia),phi(90e-3/dx:70e-3/dx:650e-3/dx,i));
+%         legend("prony","analitico");
+%         hold off;
+%     end
 
 
 
